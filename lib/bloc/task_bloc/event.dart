934a88f5170cf31eae484514dc8071151f2e2fd6
alish <alishakoori89@gmail.com>
2022-09-24 09:55:38 +0330

    ed

--- lib/bloc/task_bloc/event.dart
+++ lib/bloc/task_bloc/event.dart
@@ -1,35 +1,3 @@
-// abstract class TasksEvent {
-//   const TasksEvent();
-// }
-//
-// class GetAllTaskEvent extends TasksEvent {
-//   const GetAllTaskEvent();
-// }
-//
-// class AddNewTaskEvent extends TasksEvent {
-//   final String title;
-//   final String? description;
-//   final bool? done;
-//
-//   AddNewTaskEvent(
-//       {required this.title, this.description, this.done});
-//
-//   @override
-//   List<Object> get props => [title, description!, done!];
-// }
-//
-// class EditTaskEvent extends TasksEvent {
-//
-//   final int id;
-//   final bool done;
-//
-//   EditTaskEvent(
-//       {required this.id, required this.done});
-//
-//   @override
-//   List<Object> get props => [id, done];
-// }
-
 import 'package:equatable/equatable.dart';
 
 abstract class TasksEvent{
